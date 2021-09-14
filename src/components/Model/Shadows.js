ac8fffa542c7260541168b3ec6695c6d4dafd11b
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-09-14 02:56:39 -0500

    Fix model shadows.

--- src/components/Model/Shadows.js
+++ src/components/Model/Shadows.js
@@ -1,6 +1,9 @@
 import { useRef, useEffect, useCallback } from 'react';
 import {
   WebGLRenderTarget,
+  PlaneGeometry,
+  MeshBasicMaterial,
+  Mesh,
   OrthographicCamera,
   MeshDepthMaterial,
   ShaderMaterial,
@@ -18,15 +21,22 @@ const shadowDarkness = 3;
 
 const Shadows = () => {
   const { gl, scene, camera } = useThree();
-  const group = useRef();
+  const shadowGroup = useRef();
   const renderTarget = useRef();
   const renderTargetBlur = useRef();
   const shadowCamera = useRef();
   const depthMaterial = useRef();
   const horizontalBlurMaterial = useRef();
   const verticalBlurMaterial = useRef();
+  const plane = useRef();
+  const blurPlane = useRef();
+  const fillPlane = useRef();
 
   useEffect(() => {
+    // The shadow container, if you need to move the plane just move this
+    shadowGroup.current.position.set(0, 0, -0.8);
+    shadowGroup.current.rotateX(Math.PI / 2);
+
     // The render target that will show the shadows in the plane texture
     renderTarget.current = new WebGLRenderTarget(renderTargetSize, renderTargetSize);
     renderTarget.current.texture.generateMipmaps = false;
@@ -34,6 +44,37 @@ const Shadows = () => {
     // The render target that we will use to blur the first render target
     renderTargetBlur.current = new WebGLRenderTarget(renderTargetSize, renderTargetSize);
     renderTargetBlur.current.texture.generateMipmaps = false;
+
+    // Make a plane and make it face up
+    const planeGeometry = new PlaneGeometry(planeWidth, planeHeight).rotateX(Math.PI / 2);
+
+    const planeMaterial = new MeshBasicMaterial({
+      map: renderTarget.current.texture,
+      opacity: shadowOpacity,
+      transparent: true,
+    });
+
+    plane.current = new Mesh(planeGeometry, planeMaterial);
+    // The y from the texture is flipped!
+    plane.current.scale.y = -1;
+    shadowGroup.current.add(plane.current);
+
+    // The plane onto which to blur the texture
+    blurPlane.current = new Mesh(planeGeometry);
+    blurPlane.current.visible = false;
+    shadowGroup.current.add(blurPlane.current);
+
+    // The plane with the color of the ground
+    const fillMaterial = new MeshBasicMaterial({
+      color: 0xffffff,
+      opacity: 0,
+      transparent: true,
+    });
+
+    fillPlane.current = new Mesh(planeGeometry, fillMaterial);
+    fillPlane.current.rotateX(Math.PI);
+    fillPlane.current.position.y -= 0.00001;
+    shadowGroup.current.add(fillPlane.current);
 
     // The camera to render the depth material from
     shadowCamera.current = new OrthographicCamera(
@@ -46,7 +87,7 @@ const Shadows = () => {
     );
     // Get the camera to look up
     shadowCamera.current.rotation.x = Math.PI / 2;
-    group.current.add(shadowCamera.current);
+    shadowGroup.current.add(shadowCamera.current);
 
     // Like MeshDepthMaterial, but goes from black to transparent
     depthMaterial.current = new MeshDepthMaterial();
@@ -73,27 +114,26 @@ const Shadows = () => {
 
   const blurShadow = useCallback(
     amount => {
-      const blurPlane = group.current.getObjectByName('blur plane');
-
-      blurPlane.visible = true;
+      blurPlane.current.visible = true;
 
       // Blur horizontally and draw in the renderTargetBlur
-      blurPlane.material = horizontalBlurMaterial.current;
-      blurPlane.material.uniforms.tDiffuse.value = renderTarget.current.texture;
+      blurPlane.current.material = horizontalBlurMaterial.current;
+      blurPlane.current.material.uniforms.tDiffuse.value = renderTarget.current.texture;
       horizontalBlurMaterial.current.uniforms.h.value = amount * (1 / 256);
 
       gl.setRenderTarget(renderTargetBlur.current);
-      gl.render(blurPlane, shadowCamera.current);
+      gl.render(blurPlane.current, shadowCamera.current);
 
       // Blur vertically and draw in the main renderTarget
-      blurPlane.material = verticalBlurMaterial.current;
-      blurPlane.material.uniforms.tDiffuse.value = renderTargetBlur.current.texture;
+      blurPlane.current.material = verticalBlurMaterial.current;
+      blurPlane.current.material.uniforms.tDiffuse.value =
+        renderTargetBlur.current.texture;
       verticalBlurMaterial.current.uniforms.v.value = amount * (1 / 256);
 
       gl.setRenderTarget(renderTarget.current);
-      gl.render(blurPlane, shadowCamera.current);
+      gl.render(blurPlane.current, shadowCamera.current);
 
-      blurPlane.visible = false;
+      blurPlane.current.visible = false;
     },
     [gl]
   );
@@ -101,6 +141,10 @@ const Shadows = () => {
   // Handle render passes
   useFrame(() => {
     const blurAmount = 5;
+
+    // Remove the background
+    const initialBackground = scene.background;
+    scene.background = null;
 
     // Force the depthMaterial to everything
     // cameraHelper.visible = false;
@@ -121,26 +165,12 @@ const Shadows = () => {
 
     // Reset and render the normal scene
     gl.setRenderTarget(null);
+    scene.background = initialBackground;
 
     gl.render(scene, camera);
   }, 1);
 
-  return (
-    <group ref={group} position={[0, 0, -8]} rotation-x={Math.PI / 2}>
-      <mesh name="blur plane" scale-y={-1} rotation-x={Math.PI / 2}>
-        <planeGeometry args={[planeWidth, planeHeight]} />
-        <meshBasicMaterial
-          transparent
-          opacity={shadowOpacity}
-          map={renderTarget.current?.texture}
-        />
-      </mesh>
-      <mesh name="fill plane" position-y={0.00001} rotation-x={Math.PI / 2}>
-        <planeGeometry args={[planeWidth, planeHeight]} />
-        <meshBasicMaterial transparent opacity={0} />
-      </mesh>
-    </group>
-  );
+  return <group ref={shadowGroup} />;
 };
 
 export default Shadows;
